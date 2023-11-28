4f1a27c1e13e357673ddb283403367de4b9bc15d
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2023-11-28 01:37:53 -0800

    add product to database

--- frontend/src/app/admin/admin.component.ts
+++ frontend/src/app/admin/admin.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-admin',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent {
+  productForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private productService: ProductService) {
+    
+    this.productForm = this.fb.group({
+      product_name: ['', Validators.required],
+      description: ['', Validators.required],
+      price: ['', [Validators.required, Validators.min(0)]],
+      image: ['', Validators.required]
+      
+    });
+  }
+
+  createProduct() {
+    if (this.productForm.valid) {
+      const productData = this.productForm.value;
+
+      this.productService.createProduct(productData).subscribe(
+        (response) => {
+          console.log('Product created successfully', response);
+          this.productForm.reset();
+        },
+        (error) => {
+          console.error('Error creating product', error);
+        }
+      );
+    } else {
+      console.error('Form is not valid');
+    }
+  }
 }
